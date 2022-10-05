b01f411dc2e56db2eb59eac8301b4fce43fb65a4
sud20010709 <113490955+sud20010709@users.noreply.github.com>
2022-10-05 09:29:21 +0530

    Add files via upload

---
+++ SpringBootOnAWS/src/main/java/com/test/SpringBootOnAwsApplication.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootOnAwsApplication {
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to AWS First Deployment ... DEPLOY SPRINGBOOT TO AWS";
+		
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOnAwsApplication.class, args);
+	}
+
+}
